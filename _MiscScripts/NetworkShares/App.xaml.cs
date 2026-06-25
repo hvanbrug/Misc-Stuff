0bcd4a448a1ec358f51ac01f6949ae0329f00ef7
Henk vanBruggen <henk.vanbruggen@microsurvey.com>
2026-06-25 08:17:27 -0700

    Added a dark theme

--- _MiscScripts/NetworkShares/App.xaml.cs
+++ _MiscScripts/NetworkShares/App.xaml.cs
@@ -4,4 +4,10 @@ namespace NetworkShares;
 
 public partial class App : Application
 {
+  protected override void OnStartup( StartupEventArgs e )
+  {
+    // Populate the themed brush resources before the main window is created.
+    ThemeManager.Initialize();
+    base.OnStartup( e );
+  }
 }
